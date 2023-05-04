ad21d905bb7f297f7ed156005f9a367b036bd705
ieiriyuki <ieiriyuki@example.com>
2023-05-04 20:34:03 +0900

    feat: query a row

--- app/tutorial/03_dbaccess/main.go
+++ app/tutorial/03_dbaccess/main.go
@@ -42,6 +42,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
 }
 
 func albumsByArtist(name string) ([]Album, error) {
@@ -63,4 +69,17 @@ func albumsByArtist(name string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumByID %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumByID %d: %v", id, err)
+	}
+	return alb, nil
 }
