5602a062425cbb51553a8db2eeaf2ece6b5650aa
ieiriyuki <ieiriyuki@example.com>
2023-05-04 20:16:32 +0900

    feat: connect mysql from go

---
+++ app/tutorial/03_dbaccess/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"database/sql"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID int64
+	Title string
+	Artist string
+	Price float32
+}
+
+func main() {
+	cfg := mysql.Config{
+		User: os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net: "tcp",
+		Addr: "db:3306",
+		DBName: "go",
+	}
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected")
+}
+
